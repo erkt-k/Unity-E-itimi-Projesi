9c6d6e4d31cb182c558edae91b14b2583e07d83b
Erkt-k <erktkilinc@gmail.com>
2024-12-10 17:26:43 +0300

    Initial commit

---
+++ Assets/Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    /*Bu dersimizde bir oyuncu karakterini hareket ettirirken 'Transform Component'ini
+    * Kullanmayı ve ona özel bazı methodları öğreneceğiz. 
+    * Sonrasında input almayı ve oyuncuyu sınırlar içerisinde hareket ettirmeyi öğreneceğiz.
+    *
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity-E-itimi-Projesi
+ The final project of my lesson series on beginner level Unity.
